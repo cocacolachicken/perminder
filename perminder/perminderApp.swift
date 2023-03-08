9f34a8e8d1a08c4802ef66e855afb0c45e67a575
tyler <tyler.gu@ashbury.ca>
2023-03-08 13:55:17 -0500

    Initial Commit

---
+++ perminder/perminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  perminderApp.swift
+//  perminder
+//
+//  Created by Tyler Gu on 2023-03-08.
+//
+
+import SwiftUI
+
+@main
+struct perminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
