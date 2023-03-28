18fd701d1ab785093a0430994b418916e61cdfc8
leqishen <leqi.shen@ashbury.ca>
2023-03-28 08:23:05 -0400

    removed "subject" var

--- perminder/Tag.swift
+++ perminder/Tag.swift
@@ -9,23 +9,20 @@ import Foundation
 
 class Tag:Identifiable {
     private var name:String
-    private var subject:String
     private var reminders:[Reminder] = []
     private var color:RGB // Hex
     
-    init (n:String, s:String, c:[Int]) {
+    init (n:String, c:[Int]) {
         name = n
-        subject = s
         color = RGB(red:c[0], green:c[1], blue:c[2])
     }
     
 #if DEBUG
-    static let example = Tag(n:"Example", s:"Example", c:[255, 0, 0])
+    static let example = Tag(n:"Example", c:[255, 0, 0])
     #endif
 }
 
 class CodableTag: Codable {
     var name:String
-    var subject:String
     var color:[Int]
 }
