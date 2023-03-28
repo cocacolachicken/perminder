c59d19e5699712b688aa33aeda5c3dbee3311a55
leqishen <leqi.shen@ashbury.ca>
2023-03-27 20:08:02 -0400

    reminder class complete

--- perminder/Reminder.swift
+++ perminder/Reminder.swift
@@ -10,12 +10,34 @@ import Foundation
 class Reminder {
     private var name:String
     private var created:Date
-    private var due:Date?
+    private var due:Date
     private var finished:Date?
+
     
+    init (nameIn:String, createdIn:Date, dueIn:Date, finishedIn:Date? = nil) {
+        name = nameIn
+        created = createdIn
+        due = dueIn
+        finished = finishedIn
+    }
     
-    init (n:String) {
-        name = n
-        created = Date()
+    public func setName(nameSet:String) {
+        name = nameSet
+    }
+    
+    public func getName() -> String {
+        return name
+    }
+    
+    public func getCreated() -> Date {
+        return created
+    }
+    
+    public func setDue(dueSet : Date) {
+        due = dueSet
+    }
+    
+    public func getDue() -> Date {
+        return due
     }
 }
