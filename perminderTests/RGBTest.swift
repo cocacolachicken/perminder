9a2c52e9180247e912867b35cc3910eb8a67cf6b
leqishen <leqi.shen@ashbury.ca>
2023-04-01 18:03:40 -0400

    added more rgb tests

--- perminderTests/RGBTest.swift
+++ perminderTests/RGBTest.swift
@@ -11,6 +11,8 @@ import XCTest
 class RGBTest: XCTestCase {
 
     var RGBTest:RGB!
+    var RGBTest2:RGB!
+    var RGBTest3:RGB!
     let testRed = 255
     let testGreen = 145
     let testBlue = 35
@@ -18,11 +20,24 @@ class RGBTest: XCTestCase {
     override func setUp() {
         super.setUp()
         RGBTest = RGB(red: testRed, green: testGreen, blue: testBlue)
+        RGBTest2 = RGB(red: testRed, green: testGreen, blue: testBlue)
+        RGBTest3 = RGB(red: 0, green: 0, blue: 0)
+
     }
 
     override func tearDown() {
         
         super.tearDown()
+    }
+    
+    func testRGBEquality() {
+        XCTAssertEqual(RGBTest, RGBTest2)
+        XCTAssertNotEqual(RGBTest, RGBTest3)
+    }
+    
+    func testRGBHash() {
+        XCTAssertEqual(RGBTest.hashValue, RGBTest2.hashValue)
+        XCTAssertNotEqual(RGBTest.hashValue, RGBTest3.hashValue)
     }
     
     func testRGBInitialization() {
