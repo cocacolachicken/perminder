1492df993efab09666beca35d8d99375a2fe91ef
leqishen <leqi.shen@ashbury.ca>
2023-04-03 09:16:46 -0400

    testing

--- perminderTests/SupportingTest.swift
+++ perminderTests/SupportingTest.swift
@@ -1,6 +1,8 @@
 //
 //  SupportingTest.swift
 //  perminderTests
+//  Tests the methods in the Supporting class.
+//  According to Tyler, decode, writeToFile, and readFromJsonFile only work in the context of the simulator, as they deal with files stored on the computer, making it easier to test through manual testing. This will be documented in the testing documents.
 //
 //  Created by Leqi Shen on 2023-04-03.
 //
@@ -9,33 +11,53 @@
 import XCTest
 
 final class SupportingTest: XCTestCase {
-    func testDecodeValidFile() {
-        // Given
-        let jsonString = """
-            {
-                "name": "Acme Inc.",
-                "founded": "2000-01-01T00:00:00Z",
-                "employees": 100
-            }
-        """
-        let jsonData = jsonString.data(using: .utf8)!
-        let fileManager = FileManager.default
-        let temporaryDirectory = fileManager.temporaryDirectory
-        let filePath = temporaryDirectory.appendingPathComponent("test.json")
-        try! jsonData.write(to: filePath)
+    
+    var testBund:Bundle!
+    
+    override func setUp() {
+        super.setUp()
+        testBund = Bundle()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    /**
+     * tests "encode" function and compares it against swift's default JSON encoder
+     */
+    func testEncodeString() {
+        let toBeEncoded = "Hello World!"
+        let encodedString = testBund.encode(encode: toBeEncoded)
         
-        let testBund:Bundle = Bundle()
-        let result = try! testBund.decode(file: filePath.path) as String
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        encoder.outputFormatting = .prettyPrinted
         
-        // Then
-        let expected: String =
-                    """
-                        {
-                            "name": "Acme Inc.",
-                            "founded": "2000-01-01T00:00:00Z",
-                            "employees": 100
-                        }
-                    """
-        XCTAssertEqual(result, expected)
+        let defaultEncodeString = try? encoder.encode(toBeEncoded)
+        
+        print(encodedString)
+        XCTAssertEqual(encodedString, String(data: defaultEncodeString!, encoding: .utf8))
+    }
+    
+    /**
+     * tests "encode" function with int array, simulating RGB class, and compares it against swift's default encoder
+     */
+    func testEncodeRGB() {
+        let toBeEncoded = [0, 100, 200] //uses int array to simulate RGB
+        let encodedRGB = testBund.encode(encode: toBeEncoded)
+        
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        encoder.outputFormatting = .prettyPrinted
+        
+        let defaultEncodeString = try? encoder.encode(toBeEncoded)
+        
+        print(encodedRGB)
+        XCTAssertEqual(encodedRGB, String(data: defaultEncodeString!, encoding: .utf8))
+    }
+    
+    func testFormatDate() {
+        
     }
 }
